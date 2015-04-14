6d733dc0e52499861d3ecec1256afdd0974cc3c9
ElielMVG <infasha.mvuge@gmail.com>
2015-04-14 11:38:30 +0200

    Bukavu_projet_by_MvgAndMahid
    Juste pour un debut!

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+		<script type="text/javascript"
+      			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD_M34eZg0zFUjwYltQ51lgZg3RqEkfqKc" sensor=true>
+    	</script>
+
+		<title>Bienvenu sur tout sur BUKAVU</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<link rel="icon" type="image/png" href="ico.png" />
+	</head>
+
+	<body>
+
+		<div id="banner">
+			<h1>Bienvenue sur TOUT_SUR_BUKAVU</h1>	
+		</div>
+
+		<div id="galerie">
+
+						<img src="images/slide1.jpg" alt="slide1" class="active"/>
+						<img src="images/slide2.png" alt="slide2" />
+						<img src="images/slide3.jpg" alt="slide3" />
+						<img src="images/slide4.jpg" alt="slide4" />
+						<img src="images/slide5.jpg" alt="slide5" />
+						<img src="images/slide6.jpg" alt="slide6" />
+
+		</div>
+
+		<div id="info">
+			
+			Bonjour
+
+		</div>
+
+		<div id="middle">
+					
+		</div>
+
+		<div id="foot">
+				<p id="copyright">Copyright &copy; Mahid and MVG 2015, Designed by Mahid</p>
+		</div>
+
+		<script src="js/jquery.js"></script>
+		<script src="js/slide.js"></script>
+
+	</body>
+</html>
